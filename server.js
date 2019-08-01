011b8eb36ea0d326215cb11aa34b595152aecb7a
worldpeer <contact@worldpeer.com>
2019-08-01 15:00:40 -0700

    updating code for heroku

--- server.js
+++ server.js
@@ -53,7 +53,7 @@ app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 app.get('/' , (req, res) => {
   res.send('Hello World!');
 });
-
+console.log('HEROKU WORKS')
 //___________________
 //Listener
 //___________________
